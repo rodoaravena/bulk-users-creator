552154b8db939d5420ad3b63fdb4fbb08999ccf1
Rodolfo Aravena Collipal <rodo.aravena@outlook.com>
2022-06-08 18:27:15 -0400

    dot env creado

--- pythongitlab.py
+++ pythongitlab.py
@@ -1,15 +1,14 @@
+import os
 import gitlab
 import pandas as pd
+from dotenv import load_dotenv
 
-def create_project():
-    user = gl_obj.users.list(username='rodolfo.aravena')[0]
-    user_project = user.projects.create({'name': 'project'})
-    user_projects = user.projects.list()
-    
+load_dotenv()
+
 def auth_gitlab():
     gl = None
     try:
-        gl = gitlab.Gitlab(url=uri, private_token=token)
+        gl = gitlab.Gitlab(url=os.getenv('GITLAB_URL'), private_token=os.getenv('GITLAB_PERSONAL_TOKEN'))
         gl.auth()
         
     except gitlab.GitlabAuthenticationError as e:
@@ -18,15 +17,12 @@ def auth_gitlab():
     
     return gl
 
-def bulk_users_creation(data):
+def create_project():
     gl = auth_gitlab()
     if gl is not None:
-        for idx, row in data.iterrows():
-            user = gl.users.create({
-                        'email': row.CORREO,
-                        'password': row.pswd,
-                        'username': row.usuario,
-                        'name': row.NOMBRE})
+        user = gl_obj.users.list(username='rodolfo.aravena')[0]
+        user_project = user.projects.create({'name': 'project'})
+        user_projects = user.projects.list()
 
 def bulk_users_creation(data):
     gl = auth_gitlab()
@@ -37,16 +33,19 @@ def bulk_users_creation(data):
                         'password': row.pswd,
                         'username': row.usuario,
                         'name': row.NOMBRE})
+            user.activate()
 
 def bulk_add_member_to_project():
+    # TODO: Asignar proyecto
     pass
-    # member = project.members.create({'user_id': user.id, 'access_level':
-    #                             gitlab.const.DEVELOPER_ACCESS})
-        
+    
 
-gl_obj = auth_gitlab('<your_gilab_url_here>','<your_gilab_token_here>')
 
-data = pd.read_csv('<your_csv_file_here>')
 
-bulk_users_creation(data, gl_obj)
+gl_obj = auth_gitlab()
+
+# data = pd.read_csv('<your_csv_file_here>')
+
+# bulk_users_creation(data, gl_obj)
+
 
